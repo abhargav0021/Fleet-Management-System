5bfe1edfc0e3074eeeaaffa14989b3d97ada3cf9
Bhargav Reddy <129575362+abhargav0021@users.noreply.github.com>
2025-10-08 11:16:55 -0500

    Pushing Driver Microservices

---
+++ driver-microservice/build/resources/main/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO driver (id, first_name, last_name, phone_number,vehicle_id) VALUES
+  (0, 'Gigi', 'Wentworth (877-CASH-NOW)', '(555) 867-5310',0),
+  (1, 'Fu', 'Duh', '(555) 867-5303',1),
+  (2, 'Joe', 'Schmo', '(555) 867-5302',2);
